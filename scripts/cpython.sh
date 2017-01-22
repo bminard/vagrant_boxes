825ccd713f48acb451dd8fc22208ab3fb12727b6
Brian Minard <bminard@flatfoot.ca>
2017-01-22 18:05:16 -0500

    Install packages required by Cpython

---
+++ scripts/cpython.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#-------------------------------------------------------------------------------
+# Install Cpython dependencies.
+#-------------------------------------------------------------------------------
+set -xe
+
+
+# Required by https://docs.python.org/devguide/setup.html.
+yum --assumeyes install mercurial
+yum --assumeyes install yum-utils
+
+
+# Required to build Cpython modules.
+yum --assumeyes install bzip2-devel
+yum --assumeyes install gdbm-devel
+yum --assumeyes install libffi-devel
+yum --assumeyes install ncurses-devel
+yum --assumeyes install openssl-devel
+yum --assumeyes install readline-devel
+yum --assumeyes install sqlite-devel
+yum --assumeyes install tk-devel
+yum --assumeyes install xz-devel
+yum --assumeyes install zlib-devel
